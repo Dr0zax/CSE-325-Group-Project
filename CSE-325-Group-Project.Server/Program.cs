d715bf2c1c8db1f24c2f15004d9eebc23173cf3b
Dr0zax <geodashfan4@gmai.com>
2026-06-27 11:34:24 -0600

    moved jwt secret to user secrets and cleaned up program.cs

--- CSE-325-Group-Project.Server/Program.cs
+++ CSE-325-Group-Project.Server/Program.cs
@@ -26,13 +26,8 @@ if (File.Exists(envFile))
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Client", policy =>
-        policy.WithOrigins("http://localhost:5059")
-            .AllowAnyHeader()
-            .AllowAnyMethod());
-});
+
+builder.Configuration.AddUserSecrets<Program>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (string.IsNullOrWhiteSpace(connectionString))
@@ -102,35 +97,8 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseCors("Client");
 app.UseRouting();
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
